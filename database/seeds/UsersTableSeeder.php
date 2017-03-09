1c44fa82891c17f55b717310380ee920e1690f1f
Dimitris Tsiktsiris <tsiktsiris@sch.gr>
2017-03-09 22:18:09 +0200

    Seed the users!

---
+++ database/seeds/UsersTableSeeder.php
@@ -0,0 +1,30 @@
+<?php
+
+use Illuminate\Database\Seeder;
+
+class UsersTableSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+
+      DB::table('users')->insert([
+          'name' => 'Dimitris Tsiktsiris',
+          'email' => 'tsiktsiris@erasmus.eu',
+          'role' => 1,
+          'password' => bcrypt('tsiktsiris'),
+      ]);
+
+      DB::table('users')->insert([
+          'name' => 'Antonis Mpantis',
+          'email' => 'mpantis@erasmus.eu',
+          'role' => 1,
+          'password' => bcrypt('mpantis'),
+      ]);
+
+    }
+}
