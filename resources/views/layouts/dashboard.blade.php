43918ca285ffb4f811a9377b8093ab89d106b105
Dimitris Tsiktsiris <tsiktsiris@sch.gr>
2017-03-23 15:22:21 +0200

    Use slimscroll plugin

--- resources/views/layouts/dashboard.blade.php
+++ resources/views/layouts/dashboard.blade.php
@@ -87,10 +87,13 @@ scratch. This page gets rid of all links and provides the needed markup only.
 
     <!-- REQUIRED JS SCRIPTS -->
 
+
     <!-- jQuery 2.1.3 -->
     <script src="{{ asset ("js/jquery-3.1.1.min.js") }}"></script>
     <!-- Bootstrap 3.3.2 JS -->
     <script src="{{ asset ("js/bootstrap.min.js") }}" type="text/javascript"></script>
+    <!-- Slimscroll plugin -->
+    <script src="{{ asset ("js/jquery.slimscroll.min.js") }}" type="text/javascript"></script>
     <!-- AdminLTE App -->
     <script src="{{ asset ("js/app.js") }}" type="text/javascript"></script>
 
