58e433fb0eda590e8632455dff5ce19cc04479ed
Dimitris Tsiktsiris <tsiktsiris@sch.gr>
2017-03-23 13:19:09 +0200

    Small fix

--- resources/views/application.blade.php
+++ resources/views/application.blade.php
@@ -1,8 +1,8 @@
 @extends('layouts.dashboard')
 
-{{ $page_title='Apply for Erasmus+' }}
-@section('content')
 
+@section('content')
+{{ $page_title='Apply for Erasmus+' }}
 
             <div class="box-header with-border">
 			<div class='col-md-6'>
@@ -13,13 +13,13 @@
 			 </div>
 			</div>
             <!-- /.box-header -->
-            <div class="box-body">           
+            <div class="box-body">
 			  {!! Form::open() !!}
                 <!-- text input -->
-																
+
 				<div class="form-group">
 				<div class='col-md-6'>
-				<div class="col-xs-8">            
+				<div class="col-xs-8">
    				<div class="form-group">
 					{!! Form::label('name','Όνομα:') !!}
 					{!! Form::text('name',null, ['class' => 'form-control']) !!}
@@ -27,97 +27,97 @@
 				<div class="form-group">
 					{!! Form::label('lastname','Επίθετο:') !!}
 					{!! Form::text('lastname',null, ['class' => 'form-control']) !!}
-				</div>		
+				</div>
 				<div class="form-group">
 					{!! Form::label('idnum','Αρ. Ταυτότητας:') !!}
 					{!! Form::text('idnum',null, ['class' => 'form-control']) !!}
 				</div>
 				<div class="form-group">
 				{!! Form::label('dob','Ημ. γέννησης:') !!}
-				{!! Form::text('dob',null, ['class' => 'form-control']) !!}			
+				{!! Form::text('dob',null, ['class' => 'form-control']) !!}
 				</div>
 				<div class="form-group">
 				{!! Form::label('pob','Τόπος γέννησης:') !!}
-				{!! Form::text('pob',null, ['class' => 'form-control']) !!}			
-				</div>							
+				{!! Form::text('pob',null, ['class' => 'form-control']) !!}
+				</div>
 				<div class="form-group">
 				{!! Form::label('resplace','Τόπος μόνιμης κατοικίας:') !!}
-				{!! Form::text('resplace',null, ['class' => 'form-control']) !!}			
-				</div>			
+				{!! Form::text('resplace',null, ['class' => 'form-control']) !!}
+				</div>
 				<div class="form-group">
 				{!! Form::label('numstreet','Οδός-Αριθμός:') !!}
-				{!! Form::text('numstreet',null, ['class' => 'form-control']) !!}			
+				{!! Form::text('numstreet',null, ['class' => 'form-control']) !!}
 				</div>
 				<div class="form-group">
 				{!! Form::label('postalcode','ΤΚ:') !!}
-				{!! Form::text('postalcode',null, ['class' => 'form-control']) !!}			
+				{!! Form::text('postalcode',null, ['class' => 'form-control']) !!}
 				</div>
 				<div class="form-group">
 				{!! Form::label('tel','Τηλέφωνο:') !!}
-				{!! Form::text('tel',null, ['class' => 'form-control']) !!}			
+				{!! Form::text('tel',null, ['class' => 'form-control']) !!}
 				</div>
 				<div class="form-group">
 				{!! Form::label('mobile','Κινητό:') !!}
-				{!! Form::text('mobile',null, ['class' => 'form-control']) !!}			
+				{!! Form::text('mobile',null, ['class' => 'form-control']) !!}
 				</div>
 				<div class="form-group">
 				{!! Form::label('mail','E-mail:') !!}
-				{!! Form::text('mail',null, ['class' => 'form-control']) !!}			
-				</div>															
+				{!! Form::text('mail',null, ['class' => 'form-control']) !!}
 				</div>
-				
 				</div>
-				
+
+				</div>
+
 				<div class='col-md-6'>
-				<div class="col-xs-8">  
+				<div class="col-xs-8">
 				 <div class="form-group">
 				{!! Form::label('typestudent','Τύπος φοιτητή:') !!} <br>
 				{{ Form::radio('typestudent', 'no') }} Πρόπτυχιακος <br>
 				{{ Form::radio('typestudent', 'no') }} Μέταπτυχιακός <br>
 				{{ Form::radio('typestudent', 'no') }} Υποψήφιος Διδάκτορας
 				</div>
-				  
-				  
+
+
 				 <div class="form-group">
 				{!! Form::label('yearun','Έτος σπουδών:') !!}
-				{!! Form::text('yearun',null, ['class' => 'form-control']) !!}			
+				{!! Form::text('yearun',null, ['class' => 'form-control']) !!}
 				</div>
 				 <div class="form-group">
 				{!! Form::label('univ','Σχολή:') !!}
-				{!! Form::text('univ',null, ['class' => 'form-control']) !!}			
+				{!! Form::text('univ',null, ['class' => 'form-control']) !!}
 				</div>
 				<div class="form-group">
 				{!! Form::label('dep','Τμήμα:') !!}
-				{!! Form::text('dep',null, ['class' => 'form-control']) !!}			
+				{!! Form::text('dep',null, ['class' => 'form-control']) !!}
 				</div>
 				<div class="form-group">
 				{!! Form::label('country','Χώρα:') !!}
-				{!! Form::text('country',null, ['class' => 'form-control']) !!}			
+				{!! Form::text('country',null, ['class' => 'form-control']) !!}
 				</div>
-				
+
 				<br>
-				<label>Διάστημα Τοποθέτησης:</label> 
+				<label>Διάστημα Τοποθέτησης:</label>
 				<input class="form-control" placeholder="Enter ..." type="text" maxlength="30" size="4dp">
-				
+
 				</br>
-				<label>Φορέας Τοποθέτησης:</label> 
+				<label>Φορέας Τοποθέτησης:</label>
 				<input class="form-control" placeholder="Enter ..." type="text" maxlength="30" size="4dp">
 				<br>
-				
+
 				{!! Form::label('past','Προηγούμενη συμμετοχή στο Erasmus+:') !!} <br>
 				 {{ Form::radio('past', 'yes') }} Ναι  <br>
 				 {{ Form::radio('past', 'no') }} Όχι
                   </div>
 				</br>
-			
-				
+
+
 				</div>
-				</div>	
+				</div>
 				<div class='col-md-12'>
-				
+
 				{!! Form::label('aboutme','Παρουσιάστε συνοπτικά τα κίνητρα/λόγους συμμετοχής σας στο πρόγραμμα:') !!}
 				{!! Form::textarea('aboutme',null, ['class' => 'form-control']) !!}
-				
+
 				<input type="submit" value="submit">
 				</div>
 			   </div>
@@ -125,7 +125,7 @@
               </form>
             </div>
             <!-- /.box-body -->
-          
-			
-				
+
+
+
 @endsection
