617a851761aca030c20d50e01aad0880d55398e3
Dimitris Tsiktsiris <tsiktsiris@sch.gr>
2017-03-23 13:14:35 +0200

    Add helper functions & make sidebar usable

--- resources/views/layouts/sidebar.blade.php
+++ resources/views/layouts/sidebar.blade.php
@@ -29,14 +29,13 @@
 
     <!-- Sidebar Menu -->
     <ul class="sidebar-menu">
-      <li class="header">HEADER</li>
-      <!-- Optionally, you can add icons to the links -->
-      <li class="active"><a href="#"><span>Link</span></a></li>
-      <li><a href="application"><span>Another Link</span></a></li>
-      <li class="treeview">
-        <a href="#"><span>Multilevel</span> <i class="fa fa-angle-left pull-right"></i></a>
+      <li class="header">MAIN NAVIGATION</li>
+      <!-- Optionally, you can add icons to the links-->
+      <li class="{!! classActivePath('home') !!}"><a href="{{ route('home') }}"><span>Home</span></a></li>
+      <li class="treeview {!! classActiveSegment(1,['application']) !!}">
+        <a href="#"><span>Erasmus+</span> <i class="fa fa-angle-left pull-right"></i></a>
         <ul class="treeview-menu">
-          <li><a href="#">Link in level 2</a></li>
+          <li class="{!! classActivePath('application') !!}"><a href="application">Application</a></li>
           <li><a href="#">Link in level 2</a></li>
         </ul>
       </li>
