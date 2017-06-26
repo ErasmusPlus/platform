f9ec3f6f1aa80c9f80a6d2777d5ce18e0c65ff12
Dimitris Tsiktsiris <tsiktsiris@sch.gr>
2017-06-27 02:01:48 +0300

    Attempt to fix authentication error 1

--- cas/cas_config.php
+++ cas/cas_config.php
@@ -3,7 +3,7 @@
   $cas_protocol  = SAML_VERSION_1_1;
   $cas_sso_server  = "sso.uowm.gr";
   $cas_port  = 443;
-  $cas_cert  = "certs/AddTrustExternalRoot.pem";
+  $cas_cert  = "./certs/AddTrustExternalRoot.pem";
   $cas_logout_app_redirect_url = "https://xxx.gr/app1";
 
 ?>
--- cas/home.php
+++ cas/home.php
@@ -2,7 +2,7 @@
   include_once('CAS.php');	
   include_once('cas_config.php');
   phpCAS::client($cas_protocol, $cas_sso_server, $cas_port, '');
-  phpCAS::setCasServerCACert($cas_cert); 	
+  phpCAS::setCasServerCACert(base_path()."cas".$cas_cert); 	
   phpCAS::handleLogoutRequests(true , 
 		               array($cas_sso_server));
   phpCAS::forceAuthentication();
