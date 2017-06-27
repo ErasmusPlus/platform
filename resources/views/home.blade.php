291ed13790c851e911b4cacc359a7df3f10312d2
Dimitris Tsiktsiris <tsiktsiris@sch.gr>
2017-06-27 14:35:42 +0300

    Clean up

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -1,24 +1,111 @@
 @extends('layouts.dashboard')
-@php ( $title='Αρχική' )
-@php ( $description='Landing page')
+@php ( $title='Υπηρεσία Erasmus+' )
+@php ( $description='')
 
 @section('content')
 <div class='row'>
   <div class='col-md-12'>
-  <h2>Welcome {{ EGuard::user()->fullname}}
-  <!-- Button UploadImage -->
+  <h2>
+  <!-- Button UploadImage 
 
 					<button type="button" class="btn btn-info btn-md" data-toggle="modal" data-target="#myModal">Add news</button>
-
+-->
 				<!-- Button UploadImage -->
   </h2>
 
 
-  </div>
+  
+
+  <p>Το ERASMUS+ είναι το νέο πρόγραμμα της Ευρωπαϊκής Επιτροπής για την εκπαίδευση, την κατάρτιση, τη νεολαία και τον αθλητισμό, που στοχεύει στην ενίσχυση των δεξιοτήτων και της απασχολησιμότητας καθώς και στον εκσυγχρονισμό των συστημάτων εκπαίδευσης, κατάρτισης και νεολαίας, σε όλους τους τομείς της Δια Βίου Μάθησης (Ανώτατη Εκπαίδευση, Επαγγελματική Εκπαίδευση και Κατάρτιση, Εκπαίδευση Ενηλίκων, Σχολική Εκπαίδευση, δραστηριότητες νεολαίας, κτλ).</p>
+<p>Τα ισχύοντα προγράμματα χρηματοδότησης της ΕΕ για την εκπαίδευση, την κατάρτιση και τη νεολαία έληξαν στο τέλος του 2013, και το νέο πρόγραμμα ERASMUS+ έχει τεθεί σε ισχύ από την <strong>1η Ιανουαρίου του 2014</strong>.</p>
+<p>Το νέο πρόγραμμα συνδυάζει όλα τα σημερινά προγράμματα της ΕΕ για την εκπαίδευση, την κατάρτιση και τη νεολαία όπως, μεταξύ άλλων, το ολοκληρωμένο Πρόγραμμα Δια Βίου Μάθησης (LLP) (Erasmus, Leonardo da Vinci, Comenius, Grundtvig), το πρόγραμμα «Νεολαία σε Δράση» και πέντε προγράμματα διεθνούς συνεργασίας (Erasmus Mundus, Tempus, Alfa, Edulink και τα προγράμματα συνεργασίας με τις βιομηχανικές χώρες).</p>
+<p>Για πρώτη φορά το Erasmus+ θα προσφέρει υποστήριξη στον αθλητισμό, ειδικά το μαζικό. Το νέο Πρόγραμμα ολοκληρώνεται με την ειδική Δράση του Jean Monnet, που ενθαρρύνει τη διδασκαλία, την έρευνα και τη συζήτηση στους τομείς της ιστορίας, της πολιτικής, της οικονομίας και του Ευρωπαϊκού Δικαίου.</p>
+<p>Το Πανεπιστήμιο Δυτικής Μακεδονίας είναι κάτοχος του Πανεπιστημιακού Χάρτη Erasmus 2014-2020.</p>
+<p><a href="https://uowm.gr/wp-content/uploads/2016/02/Erasmus_Charter_2014-2020_UOWM.pdf" rel="">Erasmus Charter Πανεπιστημίου Δυτικής Μακεδονίας</a></p>
+<p><strong>Institutional Erasmus Code</strong>: G KOZANI02</p>
+<p><a href="https://uowm.gr/wp-content/uploads/2016/02/Erasmus_PolicyStatement_Overall_Strategy.pdf" rel="">Erasmus Policy Statement</a></p>
+<p>Οι <strong>επιλέξιμες χώρες</strong> στο Πρόγραμμα Erasmus+ χωρίζονται σε δύο ομάδες:</p>
+<ol>
+<li><strong>Οι Χώρες που συμμετέχουν στο Πρόγραμμα (Programme Countries)</strong><br />
+Οι ακόλουθες χώρες, συμπεριλαμβανομένων και των υπερπόντιων χωρών και των περιοχών των χωρών κρατών μελών της ΕΕ , μπορούν να συμμετέχουν πλήρως σε όλες τις δράσεις του Erasmus+:</p>
+<ul>
+<li><strong>Τα κράτη μέλη της Ευρωπαϊκής Ένωσης (ΕΕ)</strong><br />
+Αυστρία, Εσθονία, Ιταλία, Πορτογαλία,Βέλγιο, Φινλανδία, Λετονία, Ρουμανία,Βουλγαρία, Γαλλία, Λιθουανία, Σλοβακία,Κροατία, Γερμανία, Λουξεμβούργο, Σλοβενία,Κύπρος, Ελλάδα, Μάλτα, Ισπανία, Δημοκρατία της Τσεχίας, Ολλανδία, Ουγγαρία, Σουηδία, Δανία, Ιρλανδία, Πολωνία, Ηνωμένο Βασίλειο.</li>
+<li><strong>Xώρες που δεν ανήκουν στην Ευρωπαϊκή Ένωση (ΕΕ)</strong><br />
+ΠΓΔΜ, Λιχτενστάιν, Τουρκία, Ισλανδία, Νορβηγία</li>
+</ul>
+</li>
+<li><strong>Χώρες Εταίροι (Partner Countries)</strong><br />
+Οι ακόλουθες χώρες μπορούν να λάβουν μέρος σε ορισμένες δράσεις στο πλαίσιο του Erasmus+, σύμφωνα με συγκεκριμένα κριτήρια και προϋποθέσεις. Περισσότερες πληροφορίες σχετικά με τα κριτήρια αυτά μπορείτε να αναζητήσετε στον <a href="http://www.iky.gr/erasmus-plus-evrwpaiki-prosklisi/odigos-programmatos-erusmus-plus" target="_blank">Οδηγό του Προγράμματος</a>.</p>
+<ul>
+<li><strong>Γειτονικές Χώρες Εταίροι της ΕΕ (Partner Countries Neighbouring EU)</strong>
+<ul>
+<li><strong>Ανατολικές Χώρες</strong>: Αρμενία, Αζερμπαϊτζάν, Λευκορωσία, Γεωργία, Μολδαβία, Ουκρανία</li>
+<li><strong>Χώρες της νότιας Μεσογείου</strong>: Αλγερία, Αίγυπτος, Ισραήλ, Ιορδανία, Λίβανος, Λιβύη, Μαρόκο, Παλαιστίνη, Συρία, Τυνησία</li>
+<li><strong>Χώρες Δυτικών Βαλκανίων</strong>: Αλβανία, Βοσνία και Ερζεγοβίνη , Κοσσυφοπέδιο, Μαυροβούνιο, Σερβία</li>
+<li><strong>Άλλες Χώρες</strong>: Ρωσική Ομοσπονδία, Ελβετία (Μετά την ανακοίνωση της Ευρωπαϊκής Επιτροπής σχετικά με τη συμμετοχή της Ελβετίας στο Erasmus + , η Ελβετία θα συμμετέχει πλέον ως Χώρα Εταίρος (Partner Country) στο πλαίσιο του Erasmus + 2014 και όχι ως χώρα που συμμετέχει στο Πρόγραμμα, εν αναμονή σχετικής συμφωνίας μεταξύ της ΕΕ και της Ελβετίας).</li>
+<li><strong>Άλλες Χώρες Εταίροι (Other Partner Countries)</strong><br />
+Όλες οι άλλες χώρες στον κόσμο, είναι σε θέση να συμμετάσχουν σε ορισμένες ενέργειες στο πλαίσιο του Erasmus + υπό συγκεκριμένες συνθήκες . Λεπτομερή κριτήρια επιλεξιμότητας μπορείτε να αναζητήσετε στον <a href="http://www.iky.gr/erasmus-plus-evrwpaiki-prosklisi/odigos-programmatos-erusmus-plus" target="_blank">Οδηγό του Προγράμματος</a>.</li>
+</ul>
+</li>
+</ul>
+</li>
+</ol>
+<p>Το Γραφείο Διεθνών Σχέσεων και Ευρωπαϊκών Εκπαιδευτικών Προγραμμάτων παρέχει κάθε διοικητική, τεχνική και διαχειριστική βοήθεια στους φοιτητές και στα μέλη της ακαδημαϊκής κοινότητας για τη σωστή εφαρμογή και διαχείριση του προγράμματος Erasmus+.</p>
+</div>
+        </div>
+</div>
+</div><!-- row end -->
+</div><!-- inner custom column end -->
+</div><!-- doc body wrapper end -->
+<div id="k-sidebar" class="col-lg-4 col-md-4"><!-- sidebar wrapper --><div class="col-padded col-shaded"><!-- inner custom column --><ul class="list-unstyled clear-margins"><!-- widgets --><li id="text-17" class="widget-container widget_text">     <div class="textwidget"><img class="aligncenter wp-image-7093" src="https://uowm.gr/wp-content/uploads/2017/03/ErasmusPlus-30years-Circle-EN-72dpi.png" alt="" width="250" height="250" /></div>
+</li><li id="nav_menu-10" class="widget-container widget_nav_menu"><h2 class="title-widget">ΣΧΕΤΙΚΟΙ ΣΥΝΔΕΣΜΟΙ</h2><div class="menu-erasmus-container"><ul id="menu-erasmus" class="menu"><li id="menu-item-1945" class="menu-item menu-item-type-post_type menu-item-object-page current-menu-item page_item page-item-1840 current_page_item menu-item-has-children menu-item-1945"><a href="https://uowm.gr/administration/services/intrelations/erasmus/" class="menu-image-title-after"><span class="menu-image-title">Erasmus+</span></a>
+<ul class="sub-menu">
+<li id="menu-item-1933" class="menu-item menu-item-type-post_type menu-item-object-page menu-item-1933"><a href="https://uowm.gr/administration/services/intrelations/erasmus/incoming-students/" class="menu-image-title-after"><span class="menu-image-title">Incoming students / Εισερχόμενοι φοιτητές</span></a></li>
+<li id="menu-item-1941" class="menu-item menu-item-type-post_type menu-item-object-page menu-item-1941"><a href="https://uowm.gr/administration/services/intrelations/erasmus/outgoing-students-studies/" class="menu-image-title-after"><span class="menu-image-title">Εξερχόμενοι φοιτητές &#8211; Σπουδές</span></a></li>
+<li id="menu-item-1944" class="menu-item menu-item-type-post_type menu-item-object-page menu-item-1944"><a href="https://uowm.gr/administration/services/intrelations/erasmus/outgoing-students-placements/" class="menu-image-title-after"><span class="menu-image-title">Εξερχόμενοι φοιτητές &#8211; Πρακτική</span></a></li>
+<li id="menu-item-1936" class="menu-item menu-item-type-post_type menu-item-object-page menu-item-1936"><a href="https://uowm.gr/administration/services/intrelations/erasmus/didaktiko-prosopiko/" class="menu-image-title-after"><span class="menu-image-title">Διδακτικό Προσωπικό</span></a></li>
+<li id="menu-item-1943" class="menu-item menu-item-type-post_type menu-item-object-page menu-item-1943"><a href="https://uowm.gr/administration/services/intrelations/erasmus/epimorfosi-prosopikou/" class="menu-image-title-after"><span class="menu-image-title">Επιμόρφωση προσωπικού</span></a></li>
+<li id="menu-item-1939" class="menu-item menu-item-type-post_type menu-item-object-page menu-item-1939"><a href="https://uowm.gr/administration/services/intrelations/erasmus/entatika-programmata/" class="menu-image-title-after"><span class="menu-image-title">Εντατικά προγράμματα</span></a></li>
+<li id="menu-item-1938" class="menu-item menu-item-type-post_type menu-item-object-page menu-item-1938"><a href="https://uowm.gr/administration/services/intrelations/erasmus/dimereis-simfonies/" class="menu-image-title-after"><span class="menu-image-title">Διμερείς συμφωνίες</span></a></li>
+<li id="menu-item-1935" class="menu-item menu-item-type-post_type menu-item-object-page menu-item-1935"><a href="https://uowm.gr/administration/services/intrelations/erasmus/entatika-glossika-mathimata-ols/" class="menu-image-title-after"><span class="menu-image-title">Γλωσσική υποστήριξη &#8211; OLS</span></a></li>
+<li id="menu-item-1940" class="menu-item menu-item-type-post_type menu-item-object-page menu-item-1940"><a href="https://uowm.gr/administration/services/intrelations/erasmus/entatika-programmata-mikris-diarkeias/" class="menu-image-title-after"><span class="menu-image-title">Εντατικά προγράμματα Erasmus μικρής διάρκειας</span></a></li>
+<li id="menu-item-1934" class="menu-item menu-item-type-post_type menu-item-object-page menu-item-1934"><a href="https://uowm.gr/administration/services/intrelations/erasmus/atoma-me-idikes-anagkes-efpathis-omades/" class="menu-image-title-after"><span class="menu-image-title">Άτομα με ειδικές ανάγκες &#8211; Ευπαθείς Ομάδες</span></a></li>
+</ul>
+</li>
+<li id="menu-item-1937" class="menu-item menu-item-type-post_type menu-item-object-page menu-item-1937"><a href="https://uowm.gr/administration/services/intrelations/diethnis-kinitikotita-international-credit-mobility/" class="menu-image-title-after"><span class="menu-image-title">Διεθνής κινητικότητα &#8211; International Credit Mobility</span></a></li>
+<li id="menu-item-4735" class="menu-item menu-item-type-taxonomy menu-item-object-category menu-item-4735"><a href="https://uowm.gr/category/news/intrelations/" class="menu-image-title-after"><span class="menu-image-title">Ανακοινώσεις</span></a></li>
+<li id="menu-item-5918" class="menu-item menu-item-type-taxonomy menu-item-object-category menu-item-5918"><a href="https://uowm.gr/category/istories-simmetechonton-erasmus/" class="menu-image-title-after"><span class="menu-image-title">Ιστορίες συμμετεχόντων Erasmus+</span></a></li>
+<li id="menu-item-1942" class="menu-item menu-item-type-post_type menu-item-object-page menu-item-1942"><a href="https://uowm.gr/administration/services/intrelations/epikinonia/" class="menu-image-title-after"><span class="menu-image-title">Επικοινωνία</span></a></li>
+</ul></div></li><li id="text-6" class="widget-container widget_text"><h2 class="title-widget">ΠΛΗΡΟΦΟΡΙΕΣ</h2>      <div class="textwidget"><p style="text-align: center;"><img class="aligncenter size-full wp-image-1950" src="https://uowm.gr/wp-content/uploads/2016/02/erasmus-logo.png" alt="erasmus-logo" width="220" height="124" /></p>
+<hr />
+<p style="text-align: center;"><a href="http://europass.cedefop.europa.eu/el/home" target="_blank" rel="attachment wp-att-1951"><img class="aligncenter size-full wp-image-1951" src="https://uowm.gr/wp-content/uploads/2016/02/europass-logo.png" alt="europass-logo" width="220" height="62" /></a></p>
+<p style="text-align: center;"><a href="http://europass.cedefop.europa.eu/el/home" target="_blank">Ευρωπαϊκό Βιογραφικό Σημείωμα Europass</a></p>
+<hr />
+<p style="text-align: center;"><img class="aligncenter size-full wp-image-1952" src="https://uowm.gr/wp-content/uploads/2016/02/xartis_erasmus.jpg" alt="xartis_erasmus" width="200" height="238" /></p>
+<p style="text-align: center;"><a href="https://uowm.gr/wp-content/uploads/2016/02/Erasmus_Charter_2014-2020_UOWM.pdf" target="_blank" rel="">Erasmus Charter 2014-2020</a></p>
+<p style="text-align: center;"><a href="https://uowm.gr/wp-content/uploads/2016/02/Erasmus_PolicyStatement_Overall_Strategy-1.pdf" rel="">Erasmus Policy Statement (Overall Strategy)</a></p>
+<p style="text-align: center;"><a href="https://uowm.gr/wp-content/uploads/2016/06/Annex-V-β-iv-Erasmus-Student-Charter.pdf" target="_blank">Χάρτης Φοιτητή Erasmus+</a></p>
+<hr />
+<p style="text-align: center;"><a href="https://uowm.gr/wp-content/uploads/2016/02/UOWM-BILATERAL-AGREEEMENTS.pdf" rel="">Partner Universities</a></p>
+<hr />
+<p style="text-align: center;"><strong>Erasmus University Charter:</strong><br />
+221947-EPP-1-2014-1-GR-EPPKA3-ECHE</p>
+<p style="text-align: center;"><strong>Erasmus ID Code:</strong><br />
+G KOZANI02</p>
+<hr />
+<p style="text-align: center;"><a href="https://uowm.gr/wp-content/uploads/2016/02/joint-degrees-from-a-to-z-a-practical-guide-draft.pdf" target="_blank" rel="">Joint Degrees from A to Z: A practical guide</a></p>
+<hr />
+<p><img class="aligncenter wp-image-5769 size-thumbnail" src="https://uowm.gr/wp-content/uploads/2016/03/LogoIKY-150x150.png" alt="LogoIKY" width="150" height="150" /></p>
+<p style="text-align: center;"><small>Η υποστήριξη της Ευρωπαϊκής Επιτροπής για την παραγωγή της παρούσας [έκδοσης, ιστοσελίδας, ταινίας κλπ] δεν συνιστά αποδοχή του περιεχομένου, το οποίο αντανακλά τις απόψεις μόνον των δημιουργών, και η Ευρωπαϊκή Επιτροπή δεν φέρει ουδεμία ευθύνη για οποιαδήποτε χρήση των πληροφοριών που εμπεριέχονται σε αυτό</small></p>
+</div>
+</li><li id="text-3" class="widget-container widget_text"><h2 class="title-widget">ΧΡΗΣΙΜΟΙ ΣΥΝΔΕΣΜΟΙ</h2>      <div class="textwidget"><p><a href="https://uowm.gr/wp-content/uploads/2017/04/ΕΝΗΜΕΡΩΤΙΚΟΣ-ΟΔΗΓΟΣ-ΠΔΜ-2017-2018.pdf" class="custom-button" style="background-color: #72c0eb;" title="ΕΝΗΜΕΡΩΤΙΚΟΣ ΟΔΗΓΟΣ ΠΔΜ 2017-18" target="_blank"><i class="custom-button-icon fa fa-info-circle"></i><span class="custom-button-wrap"><span class="custom-button-title">ΕΝΗΜΕΡΩΤΙΚΟΣ ΟΔΗΓΟΣ ΠΔΜ 2017-18</span><span class="custom-button-tagline"> </span></span><em></em></a></p>
+
+</div>
   <div class='col-md-6'>
 
 
-    <!-- Box -->
+    <!-- 
 
 	@foreach ($news_get as $news)
     <div class="box box-primary">
@@ -33,12 +120,12 @@
       </div>
       <div class="box-body">
 	  {{ $news->body }}
-      </div><!-- /.box-body -->
+      </div>
 
-	</div><!-- /.box -->
+	</div>
    @endforeach
 
-
+  -->
 
 </div><!-- /.row -->
 
@@ -46,7 +133,7 @@
 
 </div>
 
-
+  <!--
   <div class="modal fade" id="myModal" role="dialog">
     <div class="modal-dialog modal-lg">
       <div class="modal-content">
@@ -77,7 +164,7 @@
         </div>
       </div>
     </div>
-  </div>
-
-
+  
+  -->
+</div>
 @endsection
