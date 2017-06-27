bffa7759ba581d8983685ccd2811365a2756bbac
Dimitris Tsiktsiris <tsiktsiris@sch.gr>
2017-06-27 11:56:28 +0300

    Add EGuard facade

--- config/app.php
+++ config/app.php
@@ -231,6 +231,7 @@ return [
         'Validator' => Illuminate\Support\Facades\Validator::class,
         'View' => Illuminate\Support\Facades\View::class,
         'Cas' => Subfission\Cas\CasServiceProvider::class,
+        'EGuard' => App\Classes\EGuard::class,
 		   // 'PDF' => Barryvdh\DomPDF\Facade::class,
         'Guzzle' => Kozz\Laravel\Facades\Guzzle::class,
     ],
