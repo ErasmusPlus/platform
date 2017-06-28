d0c2584a5bd6633cc3b5d2a18556250eca73853d
Dimitris Tsiktsiris <tsiktsiris@sch.gr>
2017-06-28 11:31:15 +0300

    DD path

--- app/Http/Controllers/Auth/LoginController.php
+++ app/Http/Controllers/Auth/LoginController.php
@@ -56,7 +56,7 @@ class LoginController extends Controller
         //Implement cas authentication here
         //cas()->authenticate();
         //cas()->client("S1", "https://sso.uowm.gr", "443", '');
-        
+        dd(base_path()."certs/AddTrustExternalRoot.pem");
         cas()->setCasServerCACert(base_path()."certs/AddTrustExternalRoot.pem"));
         cas()->forceAuthentication();
 
