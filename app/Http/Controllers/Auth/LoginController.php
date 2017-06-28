7c212225b732aceb3d50585c956ba54c0aa7d221
Dimitris Tsiktsiris <tsiktsiris@sch.gr>
2017-06-28 11:30:44 +0300

    Correct certificate path

--- app/Http/Controllers/Auth/LoginController.php
+++ app/Http/Controllers/Auth/LoginController.php
@@ -56,7 +56,8 @@ class LoginController extends Controller
         //Implement cas authentication here
         //cas()->authenticate();
         //cas()->client("S1", "https://sso.uowm.gr", "443", '');
-        cas()->setCasServerCACert(asset("certs/AddTrustExternalRoot.pem"));
+        
+        cas()->setCasServerCACert(base_path()."certs/AddTrustExternalRoot.pem"));
         cas()->forceAuthentication();
 
         $user = cas()->getAttributes();
