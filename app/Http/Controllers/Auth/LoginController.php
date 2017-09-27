94db2f8e52e7cceb0b67339e8dd91aa18d41110e
Dimitris Tsiktsiris <tsiktsiris@sch.gr>
2017-09-27 13:12:50 +0300

    Fix CAS logout

--- app/Http/Controllers/Auth/LoginController.php
+++ app/Http/Controllers/Auth/LoginController.php
@@ -189,13 +189,14 @@ class LoginController extends Controller
         $params = [];
         $params['service'] = "http://83.212.103.229/erasmus/public/";
         $params['url'] = "https://sso.uowm.gr/logout";
-        phpCAS::logout($params);
+        session_unset();
+        session_destroy();
         //return redirect()->route('logout');
       }
 
       //If we are CAS authenticated logout!
       //if(env('AUTH_CAS', true))
 
-      //return redirect()->route('login');
+      return redirect()->route('login');
     }
 }
