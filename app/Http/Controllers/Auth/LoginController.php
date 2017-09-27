c69725c0f663c8c3239b6bf5d274ff23a7e1d052
Dimitris Tsiktsiris <tsiktsiris@sch.gr>
2017-09-27 11:15:10 +0300

    Unset all cookies

--- app/Http/Controllers/Auth/LoginController.php
+++ app/Http/Controllers/Auth/LoginController.php
@@ -186,8 +186,16 @@ class LoginController extends Controller
 
       Auth::logout();
 
-      unset($_COOKIE['laravel_session']);
-
+      // unset cookies
+      if (isset($_SERVER['HTTP_COOKIE'])) {
+          $cookies = explode(';', $_SERVER['HTTP_COOKIE']);
+          foreach($cookies as $cookie) {
+              $parts = explode('=', $cookie);
+              $name = trim($parts[0]);
+              setcookie($name, '', time()-1000);
+              setcookie($name, '', time()-1000, '/');
+          }
+      }
       return cas()->logout();
     }
 }
