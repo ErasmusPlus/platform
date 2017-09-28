07c3be326fbf3c2d8f34160541f91a74afb63645
Dimitris Tsiktsiris <tsiktsiris@sch.gr>
2017-09-28 11:31:11 +0300

    Revert "Remove Auth"
    This reverts commit a3e868fcf8ffbaa663516eb4b0a8746f27b05b27.

--- app/Classes/EGuard.php
+++ app/Classes/EGuard.php
@@ -50,7 +50,7 @@ class EGuard
 
     public static function authenticated()
     {
-        if(Session()->get('current_user'))
+        if(Session()->get('current_user') || Auth::user())
             return true;
 
         return false;
