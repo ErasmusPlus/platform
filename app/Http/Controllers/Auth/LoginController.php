a54812adaa9898166a3b2c05048926cecaf270d0
Dimitris Tsiktsiris <tsiktsiris@sch.gr>
2017-09-28 09:28:41 +0300

    Call logout twice

--- app/Http/Controllers/Auth/LoginController.php
+++ app/Http/Controllers/Auth/LoginController.php
@@ -179,7 +179,7 @@ class LoginController extends Controller
       EGuard::logout();
       Auth::logout();
 
-
+      \Route::dispatch(\Request::create('logout', 'GET'));
 	//$cookie = Cookie::forget('laravel_session');
       Session::flush();
 //Cookie::queue('laravel_session', null, -1);
@@ -190,8 +190,8 @@ class LoginController extends Controller
 
 
 
-      //if(cas()->isAuthenticated())
-      //cas()->logout();
+      if(cas()->isAuthenticated())
+      cas()->logout();
       /*
       Cookie::queue(Cookie::forget('laravel_session','/'));
 	   Cookie::queue(Cookie::forget('CASAuth','/'));
