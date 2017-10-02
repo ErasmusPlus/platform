3eba55f4cd99dab0957fc78c8f3f9d6722ddf24d
Dimitris Tsiktsiris <tsiktsiris@sch.gr>
2017-10-02 11:00:46 +0300

    Update University model

--- app/University.php
+++ app/University.php
@@ -6,5 +6,20 @@ use Illuminate\Database\Eloquent\Model;
 
 class University extends Model
 {
-    //
+
+    public function CurrAppls1()
+    {
+        return hasMany('App\Application','u1_id','id');
+    }
+
+    public function CurrAppls2()
+    {
+        return hasMany('App\Application','u2_id','id');
+    }
+
+    public function CurrAppls3()
+    {
+        return hasMany('App\Application','u3_id','id');
+    }
+
 }
