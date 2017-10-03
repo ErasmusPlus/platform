5de92ce43f095dd13079e7ed805445e34c4090ac
Dimitris Tsiktsiris <tsiktsiris@sch.gr>
2017-10-03 09:25:08 +0300

    Hold ranks in database

---
+++ database/migrations/2017_10_03_062037_create_ranks_table.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateRanksTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('ranks', function (Blueprint $table) {
+            $table->increments('id');
+            $table->integer('uni_id')->unsigned()->nullable();
+            $table->foreign('uni_id')->references('id')->on('universities')->onDelete('set null');
+            $table->integer('app_id')->unsigned()->nullable();
+            $table->foreign('app_id')->references('id')->on('applications')->onDelete('set null');
+            $table->decimal('pts',5,2)->nullable();
+            $table->smallInteger('year');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('ranks');
+    }
+}
