d7b35d6e00b138bef9b9a83935f16078f69f6873
Dimitris Tsiktsiris <tsiktsiris@sch.gr>
2017-10-17 13:52:50 +0300

    Show correctly languages & level

--- resources/views/erasmus/viewapplicationid.blade.php
+++ resources/views/erasmus/viewapplicationid.blade.php
@@ -19,14 +19,14 @@
     @foreach($appv as $app)
      <div class="panel-heading">Αίτηση με αριθμό </div>
 		<div class="panel-body">
-			
+
                 <!-- text input -->
 
 				<div class="form-group">
-				
+
 				<div class='col-md-6'>
 				<div class="col-xs-8">
-				
+
 				<div class="form-group">
 					{!! Form::label('Επώνυμο:') !!}
 					{{$app->surname_el}}
@@ -60,156 +60,156 @@
 				{!! Form::label('mobile','Κινητό:') !!}
 				{{$app->mobtel}}
 				</div>
-				
-				
-				
-				
+
+
+
+
 				<div class="form-group">
 				{!! Form::label('Νομός/Prefecture:') !!}
 				{{$app->prefecture}}
 				</div>
-				
+
 					<div class="form-group">
 				{!! Form::label('Οδός:') !!}
 				</div>
-				
-				
-					
-				
 
 
-				
+
+
+
+
+
 				</div>
 				</div>
-				
+
 				<div class='col-md-6'>
 				<div class="col-xs-8">
-				
+
 				<div class="form-group">
 				{!! Form::label('Όνομα:') !!}
 				{{$app->name_el}}
 				</div>
-				
+
 				<div class="form-group">
 				{!! Form::label('Name (όπως αναγράφεται στο διαβατήριό σας):') !!}
 				{{$app->name_en}}
 				</div>
-				
+
 				<div class="form-group">
 				{!! Form::label('Όνομα μητρός/Mother\'s Name:') !!}
 				{{$app->mothersname}}
 				</div>
-				
+
 				<div class="form-group">
 				{!! Form::label('Ηλικία/Age:') !!}
 				{{$app->age}}
 				</div>
-								
+
 				<div class="form-group">
 				{!! Form::label('Ημερομηνία γεννήσεως/Birth of Birth:') !!}
 				{{$app->birthdate}}
 				</div>
-				
+
 				<div class="form-group">
 				{!! Form::label('Υπηκοότητα/Citizenship:') !!}
 				{{$app->citizenship}}
-				</div>			
-				
+				</div>
+
 					<div class="form-group">
 				{!! Form::label('Πόλη:') !!}
 				{{$app->city_el}}
 				{{$app->name_en}}
-				</div>	
-				
-				
-				
+				</div>
+
+
+
 					<div class="form-group">
 				{!! Form::label('Address (με λατινικούς χαρακτήρες):') !!}
 				{{$app->name_el}}
 				{{$app->name_en}}
-				</div>	
+				</div>
 
-					
-				
+
+
 					<div class="form-group">
 				{!! Form::label('Ασφαλιστικός Οργανισμός που θα καλύπτει την ιατροφαρμακευτική περίθαλψη του υποψηφίου στο εξωτερικό:') !!}
 				{{$app->insurance}}
-				</div>	
-				
-	
-				
+				</div>
+
+
+
 				</div>
 				</div>
-				
+
 				<br></br>
-				
+
 				<div class="container">
 				<div class="form-group">
 				{!! Form::label('Συμφωνώ να δίνεται το τηλέφωνό μου σε περίπτωση που ζητηθεί από άλλους φοιτητές ERASMUS:') !!}
 				{{$app->publictel}}
 				</div>
-				
+
 				<div class="form-group">
 				{!! Form::label(' 		Συμφωνώ να δίνεται το email μου σε περίπτωση που ζητηθεί από άλλους φοιτητές ERASMUS: ') !!}
 				{{$app->l1== true ? 'NAI':'OXI' }}
-				</div>	
+				</div>
 
 				<div class="form-group">
 				{!! Form::label(' Είμαι άτομο με αναπηρίες, σύμφωνα με τον κατάλογο που χρησιμοποιεί η Ε.Μ: ') !!}
 				{{$app->l2== true ? 'NAI':'OXI' }}
-				</div>	
-				
+				</div>
+
 				<div class="form-group">
 				{!! Form::label(' Έχω συμπληρώσει το 25ο έτος της ηλικίας και το ατομικό μου εισόδημα δεν ξεπερνά τα 9.000Ε: ') !!}
 				{{$app->l3== true ? 'NAI':'OXI' }}
-				</div>	
-				
-					
+				</div>
+
+
 				<div class="form-group">
 				<p>{!! Form::label(' Δεν έχω συμπληρώσει το 25ο έτος της ηλικίας και προέρχομαι απο οικογένεια που το οικογενειακό εισόδημα δεν υπερβαίνει τις 9.000Ε κατά το πλέον πρόσφατο οικονομικό έτος:') !!}</p>
 				{{$app->l4== true ? 'NAI':'OXI' }}
-				</div>	
-				
-					
+				</div>
+
+
 				<div class="form-group">
 				{!! Form::label('Προέρχομαι απο πολύτεκνη οικογένεια (4 τέκνα και άνω τα οποία βρίσκονται σε κάποια βαθμίδα εκπαίδευσης) και το οικογενειακό και ατομικό μου εισόδημα δεν υπερβαίνει τις 22.000Ε κατά το πλέον πρόσφατο οικονομικό έτος:') !!}
 				{{$app->l5== true ? 'NAI':'OXI' }}
-				</div>	
-				
+				</div>
+
 				<div class="form-group">
 				{!! Form::label(' Έχω ακυρώσει τη συμμετοχή μου στο Πρόγραμμα Σπουδών LLP/ERASMUS κατά τα προηγούμενα ακαδημαικά έτη:') !!}
 				{{$app->l6 == true ? 'NAI':'OXI' }}
 				</div>
 
 				</div>
-	
+
 
 
 				<div class="container">
-  <h2>Ξένες γλώσσες και επίπεδο γλωσσομάθειας</h2>            
+  <h2>Ξένες γλώσσες και επίπεδο γλωσσομάθειας</h2>
   <table class="table">
     <thead>
       <tr>
         <th>Ξένη Γλώσσα</th>
         <th>Επίπεδο Γλωσσομάθειας</th>
-  
+
       </tr>
     </thead>
     <tbody>
       <tr>
-        <td>{{$app->lang_id1}}</td>
-		<td>{{$app->langlevel1}}</td>
-     
+        <td>{{$languages[$app->lang_id1]}}</td>
+		<td>{{$langlevel[$app->langlevel1}}</td>
+
       </tr>
       <tr>
-           <td>{{$app->lang_id2}}</td>
-		<td>{{$app->langlevel2}}</td>
-       
+        <td>{{$languages[$app->lang_id1]}}</td>
+		<td>{{$langlevel[$app->langlevel1}}</td>
+
       </tr>
       <tr>
-          <td>{{$app->lang_id3}}</td>
-		<td>{{$app->langlevel3}}</td>
-  
+        <td>{{$languages[$app->lang_id1]}}</td>
+		<td>{{$langlevel[$app->langlevel1}}</td>
+
       </tr>
     </tbody>
   </table>
@@ -217,7 +217,7 @@
 
 
 <div class="container">
-  <h2>Πανεπιστήμια:</h2>            
+  <h2>Πανεπιστήμια:</h2>
   <table class="table">
 	<col width="250">
 	<col width="250">
@@ -251,17 +251,17 @@
     </tbody>
   </table>
 </div>
-				
 
-			
-				
+
+
+
 </div>
 
 
 </div>
   @endforeach
 
- 
+
 </div>
 
 @endsection
