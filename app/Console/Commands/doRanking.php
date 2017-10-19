3c479a194f9d12cc44b23bbf9ed4d610e6bc1c2c
Dimitris Tsiktsiris <tsiktsiris@sch.gr>
2017-10-19 14:09:23 +0300

    Fix typo

--- app/Console/Commands/doRanking.php
+++ app/Console/Commands/doRanking.php
@@ -4,7 +4,7 @@ namespace App\Console\Commands;
 
 use Illuminate\Console\Command;
 use App\Application;
-use App\university;
+use App\University;
 use App\Rank;
 use Illuminate\Database\Eloquent\Collection;
 use DB;
