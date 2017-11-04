e27e0b56f33113070270d7e35154c965288336d4
Ntonis <antonismpan@gmail.com>
2017-11-05 00:59:01 +0200

    fix if
    fix if

--- resources/views/erasmus/viewapplicationid.blade.php
+++ resources/views/erasmus/viewapplicationid.blade.php
@@ -299,16 +299,18 @@ font-weight: bold;
 {!! Form::close() !!}
 
 <!--Edit Application -->
+
 <!-- AKYROSH EGKRISHS -->
+@if( EGuard::user()-> type == 'Undergraduate')
 {!! Form::open(array('action' => ('ApplicationController@edit'))) !!}
 {{Form::hidden('id', $application->id)}}
 <div class='form-inline'>
-@if( EGuard::user()-> type == 'Undergraduate')
+
 {!! Form::submit('Επεξεργασία Αίτησης',array('class' => 'btn btn-info pull-right')) !!}
-@endif
+
 
 {!! Form::close() !!}
-
+@endif
 
 
 
