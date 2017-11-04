76d09b33791022f940f7b4b47df9d46d16b05da6
Ntonis <antonismpan@gmail.com>
2017-11-05 00:29:26 +0200

    Revert "test"
    This reverts commit c076ef8ef72a3699dc89726836f359c9a2ce84b7.

--- resources/views/admin/application/edit.blade.php
+++ resources/views/admin/application/edit.blade.php
@@ -166,7 +166,7 @@
 				   <td>
   
 		 
-            {!! Form::checkbox('publictel', {{ if ($application->publictell) != 0 echo true }} ) !!}           
+            {!! Form::checkbox('publictel', {{ if (application->publictell) != 0 echo true }} ) !!}           
 		   {!! Form::label('publictel','Συμφωνώ να δίνεται το τηλέφωνό μου σε περίπτωση που ζητηθεί από άλλους φοιτητές ERASMUS') !!}
                     
 
