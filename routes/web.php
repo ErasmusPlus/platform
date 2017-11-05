e23bcda3338920e7bef4803713d46be2c8e1e30b
Ntonis <antonismpan@gmail.com>
2017-11-05 20:30:10 +0200

    Revert "test"
    This reverts commit 4924ab8ab7b83b9da91dcc56b85a6fffa0e3534e.

--- routes/web.php
+++ routes/web.php
@@ -50,8 +50,8 @@ Route::group(['middleware' => ['cas.guard']], function () {
   Route::get('/erasmus/getpdf','ViewApplicationController@getPDF');
 
   Route::get('/profile', function () {
-      return view('profile1');
-  })->name('profile1');
+      return view('profile');
+  })->name('profile');
 
   Route::get('/settings', function () {
       return view('settings');
